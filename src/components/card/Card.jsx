bfa71f15463f48d8d311f795c6b5c5634fb20b7f
Julieth Sarmiento <jasa1999@hotmail.com>
2025-03-21 07:15:53 -0500

    fix: solucionado historico de ramas

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,12 @@
+import Image from '../ui/images/Image';
+
+const Card = ({ product }) => {
+  return (
+    <div className='p-4 bg-white shadow-md rounded-md'>
+      {product}
+      <Image src={product.image} alt={product.name} className='w-full h-64 object-cover' />
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/ui/images/Image.jsx
@@ -0,0 +1,7 @@
+const Image = ({ src, alt, ...rest }) => {
+  return (
+    <img src={src} alt={alt} {...rest} />
+  );
+}
+
+export default Image;
