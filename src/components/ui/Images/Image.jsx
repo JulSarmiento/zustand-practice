1d8e96672ff9a550f3eb96d75527bc2176a0a6da
JulSarmiento <jasa1999@hotmail.com>
2025-03-20 20:44:44 -0500

    fix: correcciones en etiquetas

--- src/components/ui/Images/Image.jsx
+++ src/components/ui/Images/Image.jsx
@@ -1,22 +1,20 @@
-const Image = ({ image, name, className }) => {
+const Image = ({ name, srcSet, src, className }) => {
 
-  console.log(image);
-  return image && image.desktop && image.tablet && image.mobile ? (
+  const srcSetString = srcSet
+    .map((image) => `${image.image} ${image.media}`)
+    .join(', ');
+
+  return (
     <figure className="flex flex-col items-center text-center">
-      <picture>
-        <source srcSet={image.desktop} media="(min-width: 1024px)" />
-        <source srcSet={image.tablet} media="(min-width: 768px)" />
-        <source srcSet={image.mobile} media="(max-width: 767px)" />
-        <img
-          src={image.desktop}
-          alt={name}
-          className={`rounded-lg shadow-md ${className}`}
-        />
-      </picture>
+      <img
+        src={src}
+        srcSet={srcSetString}
+        alt={name}
+        className={`rounded-lg shadow-md ${className}`}
+      />
     </figure>
-  ) : (
-    <p>Image not available</p>
   );
+  
 }
 
 export default Image;
